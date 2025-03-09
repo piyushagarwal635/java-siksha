120261328daf038deb82e1cedaddc2a728e9696b
piyushagarwal635 <piyushagarwal352006@gmail.com>
2025-03-09 22:54:35 +0530

    first commit

---
+++ Siksha-Setu/src/test/java/com/example/Siksha/Setu/SikshaSetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Siksha.Setu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikshaSetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
